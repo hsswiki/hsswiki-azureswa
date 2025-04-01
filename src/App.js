30b7d0a88189833fef619ae1ab7e5e2a191d7524
shen.h <shen.h@outlook.com>
2025-04-01 12:58:28 +0000

    Init profile template

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
-// import React from 'react';
-
-// function App() {
-//   const value = 'World';
-//   return <div>Hello {value}</div>;
-// }
-
-// export default App;
-
-// import React, { useState, useEffect } from 'react';
-
-// function App() {
-//   const [data, setData] = useState('');
-
-//   useEffect(() => {
-//     (async function () {
-//       // const { text } = await( await fetch(`/api/message`)).json();
-//       const text = await fetch(`/api/message`);
-//       setData(text);
-//     })();
-//   });
-
-//   return <div>{data}</div>;
-// }
-
-// export default App;
-
-import React, { useState } from 'react';
+import React from "react";
+import "./App.css";
+import Main from "./containers/Main";
+import { ThemeProvider } from "styled-components";
+import { chosenTheme } from "./theme";
+import { GlobalStyles } from "./global";
 
 function App() {
-    const [data, setData] = useState('');
-
-    const fetchData = async () => {
-        try {
-            const response = await fetch(`/api/message`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const text = await response.text();
-            setData(text);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            setData('An error occurred while fetching the data.');
-        }
-    };
-
-    return (
+  return (
+    <ThemeProvider theme={chosenTheme}>
+      <>
+        <GlobalStyles />
         <div>
-            <button onClick={fetchData}>Fetch Message</button>
-            <div>{data}</div>
+          <Main theme={chosenTheme} />
         </div>
-    );
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
